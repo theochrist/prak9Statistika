f16a82010b3d05ff3b00b0e4cb57d5d2fccc0219
theochrist <91509499+theochrist@users.noreply.github.com>
2022-11-24 20:42:48 +0700

    init

---
+++ prak9.R
@@ -0,0 +1,32 @@
+x=rnorm(100, 0, 1)
+View(x)
+hist(x)
+
+pnorm(160, 165, 6)
+
+1-pnorm(180, 165, 6)
+
+pnorm(180, 165, 6)- pnorm(160, 165, 6)
+dbinom(2, 5, 0.791462)
+
+pnorm(84, mean=72, sd=15.2, lower.tail = FALSE)
+1-pnorm(84, 72, 15.2)
+
+x=rnorm(1000, 165, 15)
+View(x)
+hist(x)
+
+x=rnorm(1000, 3.12, 0.25)
+View(x)
+hist(x)
+
+pnorm(150, 165, 6)
+
+1-pnorm(170, 165, 6)
+
+pnorm(180, 165,6) - pnorm(150, 165,6)
+dbinom(2, 7, 0.9875807)
+
+pnorm(158, 175, 30) - pnorm(125, 175, 30)
+
+pnorm(150, 175, 30, lower.tail = FALSE)
